b48bccff28b443755d4aa1acdbf6b2eb77b840a8
GetterSam <mdsamiulislam2172@gmail.com>
2023-12-14 00:41:04 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Navbar from './components/navbar';
 import MovieCard from './components/moviecard';
 import FullMovieStream from './components/fullmoviestream';
 import SignLog from './components/log-in';
-import SignIn from './components/Auth/signin';
-import SignUp from './components/Auth/signup';
 
 const App = () => {
   const isLoginPage = window.location.pathname === '/login';
@@ -17,8 +15,6 @@ const App = () => {
           <Route path="/" element={<MovieCard />} />
           <Route path="/fullmoviestream/:id" element={<FullMovieStream />} />
           <Route path="/login" element={<SignLog />} />
-          <Route path="/login2" element={<SignIn />} />
-          <Route path='/signup' element={<SignUp/>}/>
         </Routes>
       </React.Fragment>
     </BrowserRouter>
--- src/components/log-in.js
+++ src/components/log-in.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth, db } from "../Firebase/firebase";
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile  } from 'firebase/auth';
+import { setDoc, doc } from 'firebase/firestore';
 import './Styles/signlog.css';
 
 const SignLog = () => {
@@ -15,10 +18,56 @@ const SignLog = () => {
         }
     };
 
-    const handleSignupLinkClick = () => {
-        const signupbtn1 = document.querySelector("label.signup");
-        signupbtn1.click();
-    };
+    const [SignInemail, setSignInEmail] = useState("");
+    const [SignInpassword, setSignInPassword] = useState("");
+
+    const handleSignIn = (e) =>{
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, SignInemail, SignInpassword)
+        .then((userCredential) =>{console.log(userCredential)})
+        .catch((error) =>{console.log(error)})
+
+    }
+
+    const [SignUpemail, setSignUpEmail] = useState("");
+    const [SignUppassword, setSignUpPassword] = useState("");
+    const [displayName, setDisplayName] = useState("");
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        try {
+            // Create user in Firebase Authentication
+            const userCredential = await createUserWithEmailAndPassword(auth, SignUpemail, SignUppassword);
+            
+            // Update display name
+            const user = userCredential.user;
+            await updateDisplayName(user);
+
+            // document in Firestore
+            await createUserDocument(user.uid, displayName, SignUpemail);
+
+            console.log("User signup successful:", userCredential);
+        } catch (error) {
+            console.error("Error during signup:", error);
+        }
+    }
+
+    const updateDisplayName = async (user) => {
+        // Update the user's display name in Firebase
+        await updateProfile(user, { displayName: displayName });
+    }
+
+    const createUserDocument = async (uid, displayName, SignUpemail) => {
+        const userRef = doc(db, 'users', uid); // 'users' is the collection name
+        await setDoc(userRef, {
+            displayName: displayName,
+            SignUpemail: SignUpemail,
+            // store additional user information here
+        });
+
+        console.log("User document created in Firestore");
+    }
 
     return (
         <div className='container d-flex justify-content-center align-items-center vh-100'>
@@ -51,12 +100,12 @@ const SignLog = () => {
                         <div className="slider-tab"></div>
                     </div>
                     <div className="form-inner">
-                        <form action="#" className="login">
+                        <form onSubmit={handleSignIn}className="login">
                             <div className="field">
-                                <input type="email" placeholder="Email Address" required />
+                                <input type="email" placeholder="Email Address" value={SignInemail} onChange={(e) =>setSignInEmail(e.target.value)} required />
                             </div>
                             <div className="field">
-                                <input type="password" placeholder="Password" required />
+                                <input type="password" placeholder="Password" value={SignInpassword} onChange={(e) => setSignInPassword(e.target.value)} required />
                             </div>
                             <div className="pass-link">
                                 <a href="/">Forgot password?</a>
@@ -65,22 +114,20 @@ const SignLog = () => {
                                 <div className="btn1-layer"></div>
                                 <input type="submit" value="Login" />
                             </div>
-                            <div className="signup-link">
-                                Not a member? <a href="/" onClick={handleSignupLinkClick}>Signup now</a>
-                            </div>
                         </form>
-                        <form action="#" className="signup">
+
+                        <form onSubmit={handleSignUp} className="signup">
                             <div className="field">
-                                <input type="text" placeholder="User Name" required />
+                                <input type="text" placeholder="User Name" value={displayName} onChange={(e) => setDisplayName(e.target.value)} required />
                             </div>
                             <div className="field">
-                                <input type="email" placeholder="Email Address" required />
+                                <input type="email" placeholder="Email Address" value={SignUpemail} onChange={(e) => setSignUpEmail(e.target.value)} required />
                             </div>
                             <div className="field">
-                                <input type="password" placeholder="Password" required />
+                                <input type="password" placeholder="Password" value={SignUppassword} onChange={(e) => setSignUpPassword(e.target.value)} required />
                             </div>
                             <div className="field">
-                                <input type="password" placeholder="Confirm password" required />
+                                <input type="password" placeholder="Confirm password"  />
                             </div>
                             <div className="field btn1">
                                 <div className="btn1-layer"></div>
